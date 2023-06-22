66225ed3b092cfb30fecdb7f0673ddbffa564380
johaco <johaco@johaco-EXO-Smart-XQ3C>
2023-06-22 17:33:31 -0300

    Carga Iniciar

--- src/main/java/logica/Suma.java
+++ src/main/java/logica/Suma.java
@@ -11,7 +11,7 @@ public class Suma {
     public float sumar(float primer_num, float segundo_num, JLabel pantalla){
         primer_num = Float.parseFloat(pantalla.getText());
         pantalla.setText("");
-        return primer_num;
+        return primer_num; 
     }
     public String obtener_operador(String operador){
         operador = "+";
